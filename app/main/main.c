28ae60fb730da3986532eed4ad762ee70e14f6a7
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2024-05-10 12:15:19 +0200

    IT WORKS

--- app/main/main.c
+++ app/main/main.c
@@ -12,17 +12,48 @@
 
 static const char *TAG = "main";
 
-#define N_SAMPLES 2000
-int N = N_SAMPLES;
-float sum_y[N_SAMPLES / 2];
-float y_cf[N_SAMPLES * 2];
+#define WINDOWS_SIZE 512
+#define SAMPLING_FREQUENCY 1024
+int N = WINDOWS_SIZE;
+float sum_y[WINDOWS_SIZE / 2];
+float y_cf[WINDOWS_SIZE * 2];
 // Pointers to result arrays
 float *y1_cf = &y_cf[0];
-float *y2_cf = &y_cf[N_SAMPLES];
+float *y2_cf = &y_cf[WINDOWS_SIZE];
+
+void printFrequency(){
+    float y1max=0.0;
+    int index1max=0;
+    float y2max=0.0;
+    int index2max=0;
+    for(int i=0; i<N/2; i++){
+        if(y1_cf[i]>y1max){
+            y1max=y1_cf[i];
+            index1max=i;
+        }
+        
+        if(y2_cf[i]>y2max){
+            y2max=y2_cf[i];
+            index2max=i;
+        }
+        
+    }
+    printf("Y1 max => Index %d Magnitude %f\n", index1max, y1max);
+    printf("Y2 max => Index %d Magnitude %f\n", index2max, y2max);
+    float hz1=((float)index1max)*SAMPLING_FREQUENCY/N;
+    float hz2=((float)index2max)*SAMPLING_FREQUENCY/N;
+    printf("Max y1 frequency %f\n", hz1);
+    printf("Max y2 frequency %f\n", hz2);
+}
+
 void process_and_show(float *data, int length)
 {
+    
+    //float wind[N];
+    //dsps_wind_hann_f32(wind, N);
+
     for (int i = 0 ; i < N ; i++) {
-        y_cf[i * 2 + 0] = data[i];
+        y_cf[i * 2 + 0] = data[i] ;//* wind[i];
         y_cf[i * 2 + 1] = 0;
     }
     // FFT
@@ -33,21 +64,38 @@ void process_and_show(float *data, int length)
     dsps_bit_rev_fc32(y_cf, N);
     // Convert one complex vector to two complex vectors
     dsps_cplx2reC_fc32(y_cf, N);
+    
 
     for (int i = 0 ; i < N / 2 ; i++) {
         y_cf[i] = 10 * log10f((y_cf[i * 2 + 0] * y_cf[i * 2 + 0] + y_cf[i * 2 + 1] * y_cf[i * 2 + 1]) / N);
-        y_cf[i] = 10 * log10f((y2_cf[i * 2 + 0] * y2_cf[i * 2 + 0] + y2_cf[i * 2 + 1] * y2_cf[i * 2 + 1]) / N);
+        y2_cf[i] = 10 * log10f((y2_cf[i * 2 + 0] * y2_cf[i * 2 + 0] + y2_cf[i * 2 + 1] * y2_cf[i * 2 + 1]) / N);
         // Simple way to show two power spectrums as one plot
         sum_y[i] = fmax(y_cf[i], y2_cf[i]);
     }
+    
+
+    printFrequency();
+
+/*
+    printf("%f\n", y2_cf[49]);
+    printf("%f\n", y2_cf[50]);
+    printf("%f\n", y2_cf[51]);
+
+    printf("********************\n");
+    
+    printf("%f\n", y2_cf[100]);
+    printf("%f\n", y2_cf[101]);
+    printf("%f\n", y2_cf[102]);
+*/
+    printf("----------------------------------------------------------------------------------\n");
 
     // Show power spectrum in 64x10 window from -100 to 0 dB from 0..N/4 samples
     ESP_LOGW(TAG, "Signal x1");
-    dsps_view(data, N / 2, 64, 10,  -60, 40, '|');
+    dsps_view(y_cf, N / 2, 128, 10,  -60, 40, '|');
     ESP_LOGW(TAG, "Signal x2");
-    dsps_view(y2_cf, N / 2, 64, 10,  -60, 40, '|');
+    dsps_view(y2_cf, N / 2, 128, 10,  -60, 40, '|');
     ESP_LOGW(TAG, "Signals x1 and x2 on one plot");
-    dsps_view(sum_y, N / 2, 64, 10,  -60, 40, '|');
+    dsps_view(sum_y, N / 2, 128, 10,  -60, 40, '|');
     ESP_LOGI(TAG, "FFT for %i complex points take %i cycles", N, end_b - start_b);
 
     ESP_LOGI(TAG, "End Example.");
--- signal_generator_serial.py
+++ signal_generator_serial.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 # Samples per second
-sps = 1000
+sps = 1024
 
 # Frequency / pitch
-freq_hz = 3
+freq_hz = 100
 
 # Duration
 duration_s = 1.0
 
 # Attenuation so the sound is reasonable
-atten = 100.0 # 0.3
+atten = 10.0 # 0.3
 
 # NumpPy magic to calculate the waveform
 each_sample_number = np.arange(duration_s * sps)
@@ -19,6 +19,13 @@ waveform_quiet = waveform * atten
 
 f = open("array.txt", "w")
 for value in waveform_quiet:
-    f.write(str(value) + ',')
+    v = round(value,8)
+    f.write(str(v) + ',')
+f.close()
+
+f = open("array2.txt", "w")
+for value in waveform_quiet:
+    v = round(value,8)
+    f.write(str(v) + '\n')
 f.close()
 
